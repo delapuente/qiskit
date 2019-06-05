e159a4bc862dec67beaed2e372c5845d6a2fae22
SooluThomas <soolu.elto@gmail.com>
2019-06-05 15:00:55 -0400

    test travis

--- tools/deploy_documentation.sh
+++ tools/deploy_documentation.sh
@@ -66,7 +66,7 @@ git branch
 git config user.name "SooluThomas"
 git config user.email "soolu.elto@gmail.com"
 
-echo "git rm -rf"
+echo "git rm -rf for the tramslation po files"
 # git rm -rf --ignore-unmatch $DOC_DIR_2/$TRANSLATION_LANG/**/*.po
 it rm -rf --ignore-unmatch $DOC_DIR_2/$TRANSLATION_LANG/LC_MESSAGES/$TRANSLATION_LANG/*.po \
     $DOC_DIR_2/$TRANSLATION_LANG/LC_MESSAGES/$TRANSLATION_LANG/*.po \
